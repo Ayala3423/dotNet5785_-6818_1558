0ddfb2903479ea7abeccb79b7ca83819382e7808
Ayala3423 <a0548573423@gmail.com>
2024-09-16 17:45:28 +0300

    new function in Program class

---
+++ Stage0/Program1558.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage0
+{
+    internal partial class Program
+    {
+        private static void Welcome1558()
+        {
+            Console.WriteLine("jbshcbv");
+        }
+    }
+}
