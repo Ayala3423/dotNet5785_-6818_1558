114df24cd34890f25a2322c29203491086ad94b1
Ayala0533170483 <Ayala214926@gmail.com>
2024-09-16 17:08:04 +0300

    Initial Code + Welcome functions

---
+++ Stage0/Program6818.cs
@@ -0,0 +1,16 @@
+﻿internal partial class Program
+{
+     private static void Main(string[] args)
+    {
+        Welcome6818();
+        Welcome1558();
+    }
+
+    private static void Welcome6818()
+    {
+        Console.WriteLine("Enter Your Name");
+        string name = Console.ReadLine();
+        Console.WriteLine(name + ", welcome to my first console");
+    }
+    static partial void Welcome1558();
+}
